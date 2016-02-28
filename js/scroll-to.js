32e17befffa9132ae6b7c4348f6c40850967ba50
Raul Lozano Martin <raul.lozanomartin@student.manchester.ac.uk>
2016-02-28 04:24:37 +0000

    hi

--- js/scroll-to.js
+++ js/scroll-to.js
@@ -1,19 +1,13 @@
-$('#scroll-how-it-works').click(function(event) {
-    // Preventing default action of the event
-    event.preventDefault();
-    // Getting the height of the document
-    var n = $(document).height();
-    $('html, body').animate({scrollTop:$('#how-it-works').position().top}, 'slow');
 
-    this.className = "active";
+$("#scroll-how-it-works").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#how-it-works").offset().top
+    }, 2000);
 });
 
-$('#scroll-home').click(function(event) {
-    // Preventing default action of the event
-    event.preventDefault();
-    // Getting the height of the document
-    var n = $(document).height();
-    $('html, body').animate({ scrollTop: 0 }, 600);
 
-    this.className = "active";
+$("#scroll-home").click(function() {
+    $('html, body').animate({
+        scrollTop: $("html").offset().top
+    }, 2000);
 });
