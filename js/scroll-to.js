9c638d5514a4da50c8aefffc46b843508d4443e8
Raul Lozano Martin <raul.lozanomartin@student.manchester.ac.uk>
2016-03-29 10:45:04 +0100

    improved scroll function

--- js/scroll-to.js
+++ js/scroll-to.js
@@ -1,18 +1,20 @@
-
-$("#scroll-how-it-works").click(function() {
+$("#scroll-how-it-works").click(function(e) {
+	e.preventDefault();
     $('html, body').animate({
         scrollTop: $("#how-it-works").offset().top
     }, 2000);
 });
 
 
-$("#scroll-home").click(function() {
+$("#scroll-home").click(function(e) {
+	e.preventDefault();
     $('html, body').animate({
         scrollTop: $("html").offset().top
     }, 2000);
 });
 
-$("#submit-button").click(function() {
+$("#submit-button").click(function(e) {
+	e.preventDefault();
     $('html, body').animate({
         scrollTop: $("#display-results").offset().top
     }, 2000);
